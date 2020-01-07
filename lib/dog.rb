675db017abe8ffdbd46438b3b06e517f5f81c646
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-07 12:29:09 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+
+    end
+
+    def name=(dog_name)
+        @this_dog_dogs_name = dog_name
+end
+
+lassie = Dog.new
